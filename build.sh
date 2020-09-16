545e1802312d767c67e9ee519c6cdeef41afe56e
Your Name <you@example.com>
2020-09-16 23:12:51 +0800

    xxx

--- build.sh
+++ build.sh
@@ -7,17 +7,18 @@ pushd gsoap-2.8
 ./configure --prefix=$(pwd)/install
 make -j
 make install
+sed -i "s/SOAP_ENV__Fault$/SOAP_ENV__Fault_alex/g" install/share/gsoap/import/wsa5.h
 popd
 
 fi
 
 
 GSOAP_DIR=$(pwd)/gsoap-2.8/install/
-mkdir ${GSOAP_DIR}/onvif/
+mkdir ${GSOAP_DIR}/onvif/  -p
 pushd ${GSOAP_DIR}/onvif/
-${GSOAP_DIR}/bin/wsdl2h -O4 -P -x -o /tmp/onvif.h https://www.onvif.org/ver10/network/wsdl/remotediscovery.wsdl https://www.onvif.org/ver20/ptz/wsdl/ptz.wsdl
+${GSOAP_DIR}/bin/wsdl2h -O4 -P -x -o /tmp/onvif.h https://www.onvif.org/ver10/network/wsdl/remotediscovery.wsdl https://www.onvif.org/ver10/device/wsdl/devicemgmt.wsdl https://www.onvif.org/ver10/media/wsdl/media.wsdl https://www.onvif.org/ver20/ptz/wsdl/ptz.wsdl 
 ${GSOAP_DIR}/bin/soapcpp2 -2 -C -L -x -I ${GSOAP_DIR}/share/gsoap/import /tmp/onvif.h
-rm RemoteDiscoveryBinding.nsmap PTZBinding.nsmap
+rm RemoteDiscoveryBinding.nsmap PTZBinding.nsmap DeviceBinding.nsmap MediaBinding.nsmap
 popd
 
 
